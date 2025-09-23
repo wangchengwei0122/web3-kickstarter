f26770686c23e055db9288b4116ffe3b60db7377
wangchengwei <892630kux@gmail.com>
2025-09-23 22:59:38 +0800

    update: Campaign.sol

--- packages/contracts/src/Campaign.sol
+++ packages/contracts/src/Campaign.sol
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract Campaign {
-  address creator;
-  uint256 goal;
-  uint256 deadline;
-  uint256 totalPledged;
-  uint256 status;
-  mapping(address => uint256) pledges;
-  mapping(address => uint256) rewards;
-  mapping(address => uint256) milestones;
-  constructor(address _creator, uint256 _goal, uint256 _deadline) {
-    creator = _creator;
-    goal = _goal;
-    deadline = _deadline;
-    totalPledged = 0;
-    status = 0;
+contract Campaign is ReentrancyGuard {
+  enum Status {
+    Active,
+    Successful,
+    Failed,
+    Cancelled
   }
+  address creator; // 创建者
+  uint256 goal; // 目标金额
+  uint64 deadline; // 截止时间
+  Status status; // 状态
+  uint256 totalPledged; // 已筹金额
+  mapping(address => uint256) pledges; // 出资
+  mapping(address => uint256) rewards; // 奖励
+  mapping(address => uint256) milestones; // 里程碑
 }
