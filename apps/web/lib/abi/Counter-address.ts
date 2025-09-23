cc6eb857273d4f638f74b61603f5a9f1742eb27c
wangchengwei <892630kux@gmail.com>
2025-09-23 22:41:01 +0800

    feat: add openzeppelin

--- apps/web/lib/abi/Counter-address.ts
+++ apps/web/lib/abi/Counter-address.ts
@@ -1,1 +1,1 @@
-export const counterAddress = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9" as `0x${string}`;
+export const counterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3" as `0x${string}`;
