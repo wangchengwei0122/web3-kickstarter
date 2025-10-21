de0b61b9ba068564fd35c77af7a1a632341ede19
wangchengwei0122 <892630kux@gmail.com>
2025-10-21 16:09:05 +0800

    update: CampaignFactory.sol

--- packages/contracts/src/CampaignFactory.sol
+++ packages/contracts/src/CampaignFactory.sol
@@ -4,18 +4,18 @@ import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {Campaign} from "./Campaign.sol";
 
 contract CampaignFactory is Ownable {
-  address public treasury; // 资金库
-  uint16 public feeBps; // 平台费
-
-  address[] public campaigns;
-  bool public paused;
-  mapping(address => bool) public bannedCreators;
-
   event CampaignCreated(address campaign, address indexed creator, uint256 indexed id);
   event FeeBpsUpdated(uint16 oldFee, uint16 newFee);
   event TreasuryUpdated(address oldTreasury, address newTreasury);
   event Paused(bool paused);
   event CreatorBanned(address indexed creator, bool banned);
+
+  address public treasury; // 资金库
+  uint16 public feeBps; // 平台费
+  bool public paused;
+  address[] public campaigns;
+  mapping(address => bool) public bannedCreators;
+  mapping(address => address[]) public userCampaigns;
 
   constructor(address _treasury, uint16 _feeBps) Ownable(msg.sender) {
     require(_treasury != address(0), "INVALID_TREASURY");
@@ -35,10 +35,6 @@ contract CampaignFactory is Ownable {
     require(_bps <= 10_000, "FEE_TOO_HIGH");
     emit FeeBpsUpdated(feeBps, _bps);
     feeBps = _bps;
-  }
-
-  function campaignsLength() external view returns (uint256) {
-    return campaigns.length;
   }
 
   function setPaused(bool _p) external onlyOwner {
@@ -61,8 +57,19 @@ contract CampaignFactory is Ownable {
     require(goal > 0, "INVALID_GOAL");
     require(deadline > block.timestamp, "INVALID_DEADLINE");
     require(bytes(metadataURI).length > 0, "INVALID_METADATA_URI");
+
     campaign = address(new Campaign(msg.sender, address(this), goal, deadline, metadataURI));
     campaigns.push(campaign);
+    userCampaigns[msg.sender].push(campaign);
+
     emit CampaignCreated(campaign, msg.sender, campaigns.length - 1);
+  }
+
+  function campaignsLength() external view returns (uint256) {
+    return campaigns.length;
+  }
+
+  function getUserCampaigns(address user) external view returns (address[] memory) {
+    return userCampaigns[user];
   }
 }
--- packages/contracts/src/ICampaignFactory.sol
+++ packages/contracts/src/ICampaignFactory.sol
@@ -4,5 +4,10 @@ pragma solidity ^0.8.30;
 interface ICampaignFactory {
   function feeBps() external view returns (uint16);
   function treasury() external view returns (address);
-  function createCampaign(uint256 goal, uint64 deadline, string calldata metadataURI) external returns (address);
+  function createCampaign(
+    uint256 goal,
+    uint64 deadline,
+    string calldata metadataURI
+  ) external returns (address);
+  function paused() external view returns (bool);
 }
